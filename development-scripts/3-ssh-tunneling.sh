5bc2d264dc139aac114a642734b33a1df37fb51a
Timo Wang <timo@steaker.com>
2022-04-13 16:58:45 +0800

    with input prompt of vm host

--- development-scripts/3-ssh-tunneling.sh
+++ development-scripts/3-ssh-tunneling.sh
@@ -2,6 +2,10 @@
 
 echo "Run this command on your macbook."
 
-VM_HOST=<your-vm-host>
+if [[ -z "${VM_HOST}" ]]; then
+    echo "Enter your VM host:"
+    read INPUT_VM_HOST
+    export VM_HOST=${INPUT_VM_HOST}
+fi
 
 ssh -N -p 22 ${VM_HOST} -L 127.0.0.1:8080:$(ssh ${VM_HOST} "minikube ip"):30080
