830b7e1f8c4a5e32e9bf60214a590d970ee13cd2
Timo Wang <timo@steaker.com>
2022-04-13 14:31:05 +0800

    able to operate on local dev

--- development-scripts/3-port-forward.sh
+++ development-scripts/3-port-forward.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+echo "Run this command on your macbook."
+
 kubectl port-forward deployments/airflow-webserver 8080:8080
--- development-scripts/3-ssh-tunneling.sh
+++ development-scripts/3-ssh-tunneling.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Run this command on your macbook."
+
 VM_HOST=<your-vm-host>
 
 ssh -N -p 22 ${VM_HOST} -L 127.0.0.1:8080:$(ssh ${VM_HOST} "minikube ip"):30080
