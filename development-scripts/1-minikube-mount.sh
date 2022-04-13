1430dafb091fe3fe654bd76ccdee00d0e75ab76f
Timo Wang <timo@steaker.com>
2022-04-13 14:39:48 +0800

    add some echos

--- development-scripts/1-minikube-mount.sh
+++ development-scripts/1-minikube-mount.sh
@@ -2,6 +2,8 @@
 
 set -e
 if [[ $(uname) == "Linux" ]]; then
-    echo "You are on Linux. Working on minikube mount"
+    echo "You are on Linux. Working on minikube mount."
     minikube mount $(pwd):$(pwd)
+elif [[ $(uname) == "Darwin" ]]; then
+    echo "You are on Macbook. No need to run this command."
 fi
--- development-scripts/3-ssh-tunneling.sh
+++ development-scripts/3-ssh-tunneling.sh
@@ -2,6 +2,10 @@
 
 echo "Run this command on your macbook."
 
+if [[ $(uname) == "Linux" ]]; then
+    exit 1
+fi
+
 VM_HOST=<your-vm-host>
 
 ssh -N -p 22 ${VM_HOST} -L 127.0.0.1:8080:$(ssh ${VM_HOST} "minikube ip"):30080
