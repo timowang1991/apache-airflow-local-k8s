4e2031a3900a186b2a96c3aae811c509108a62dc
Timo Wang <timo@steaker.com>
2022-04-19 14:43:40 +0800

    put minikube to background

--- development-scripts/1-minikube-mount.sh
+++ development-scripts/1-minikube-mount.sh
@@ -3,7 +3,7 @@
 set -e
 if [[ $(uname) == "Linux" ]]; then
     echo "You are on Linux. Working on minikube mount."
-    minikube mount $(pwd):$(pwd)
+    minikube mount $(pwd):$(pwd) &
 elif [[ $(uname) == "Darwin" ]]; then
     echo "You are on Macbook. No need to run this command."
 fi
---
+++ development-scripts/5-kill-minikube-mount.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ $(uname) == "Darwin" ]]; then
+    echo "You are on Macbook. No need to run this command."
+    exit 0
+fi
+
+MINIKUBE_PID=$(ps -x | grep -m 1 "minikube mount $(pwd):$(pwd)" | awk -F " " '{ print $1 }')
+
+kill ${MINIKUBE_PID}
